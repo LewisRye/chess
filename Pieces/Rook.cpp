5288ab55be72175b61647f58f9a249eeb4b8ea71
Koushic <koushic.sk@yahoo.com>
2023-12-05 22:44:43 +0000

    implement rook

--- Pieces/Rook.cpp
+++ Pieces/Rook.cpp
@@ -10,6 +10,61 @@ Rook::Rook(PieceColour colour, PieceType name) : Piece(colour, name) {}
 std::vector<Move> Rook::ListPseudoLegalMoves(Board *board)
 {
   mPseudoLegalMoves.clear();
+  int row = mPosition->GetRow();
+  int col = mPosition->GetCol();
+
+  for (int i = -1; i < 2; i++) // positive --> negative
+  {
+    if (i == 0)
+    {
+      continue;
+    }
+    for (int j = 0; j < 8; j++) // normal move
+    {
+
+      int nextRow = row + i * (j + 1);
+      if (!board->InRange(nextRow, col))
+      {
+        continue;
+      }
+      Move mv = Move(board->GetSquare(row, col), board->GetSquare(nextRow, col), *board);
+      if (IsBlockedByPiece(mv) && mv.mMovingPiece->GetColour() != mv.mKilledPiece->GetColour()) // there is a piece to take
+      {
+        mPseudoLegalMoves.push_back(mv);
+      }
+      if (!IsBlockedByPiece(mv)) // not blocked by a piece
+      {
+        mPseudoLegalMoves.push_back(mv);
+      }
+      else
+      {
+        break;
+      }
+    }
+
+    for (int j = 0; j < 8; j++) // normal move
+    {
+
+      int nextCol = col - i * (j + 1);
+      if (!board->InRange(row, nextCol))
+      {
+        continue;
+      }
+      Move mv = Move(board->GetSquare(row, col), board->GetSquare(row, nextCol), *board);
+      if (IsBlockedByPiece(mv) && mv.mMovingPiece->GetColour() != mv.mKilledPiece->GetColour()) // there is a piece to take
+      {
+        mPseudoLegalMoves.push_back(mv);
+      }
+      if (!IsBlockedByPiece(mv)) // not blocked by a piece
+      {
+        mPseudoLegalMoves.push_back(mv);
+      }
+      else
+      {
+        break;
+      }
+    }
+  }
   return mPseudoLegalMoves;
 }
 
