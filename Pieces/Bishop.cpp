84e4fafe4375075382e4cd36349ebea459cb83ec
Lewis <lewisryework@gmail.com>
2023-12-05 22:27:00 +0000

    bishop implemented

--- Pieces/Bishop.cpp
+++ Pieces/Bishop.cpp
@@ -10,6 +10,50 @@ Bishop::Bishop(PieceColour colour, PieceType name) : Piece(colour, name) {}
 std::vector<Move> Bishop::ListPseudoLegalMoves(Board *board)
 {
   mPseudoLegalMoves.clear();
+  int row = mPosition->GetRow();
+  int col = mPosition->GetCol();
+
+  for (int j = -1; j < 2; j++) // up and left, down and right
+  {
+    if (j == 0) { continue; }
+    for (int i = 1; i < 8; i++)
+    {
+      if (!board->InRange(row+(i*j), col+(i*j)))
+      {
+        continue;
+      }
+      Move mv = Move(board->GetSquare(row, col), board->GetSquare(row+(i*j), col+(i*j)), *board);
+      if (IsBlockedByPiece(mv) && mv.mMovingPiece->GetColour() != mv.mKilledPiece->GetColour()) // there is one to take
+      {
+        mPseudoLegalMoves.push_back(mv);
+        break;
+      }
+      if (IsBlockedByPiece(mv)&& mv.mMovingPiece->GetColour() == mv.mKilledPiece->GetColour()) // blocked by same team
+      {
+        break;
+      }
+      mPseudoLegalMoves.push_back(mv);
+    }
+    for (int i = 1; i < 8; i++) // up and right, down and left
+    {
+      if (!board->InRange(row-(i*j), col+(i*j)))
+      {
+        continue;
+      }
+      Move mv = Move(board->GetSquare(row, col), board->GetSquare(row-(i*j), col+(i*j)), *board);
+      if (IsBlockedByPiece(mv) && mv.mMovingPiece->GetColour() != mv.mKilledPiece->GetColour()) // there is one to take
+      {
+        mPseudoLegalMoves.push_back(mv);
+        break;
+      }
+      if (IsBlockedByPiece(mv)&& mv.mMovingPiece->GetColour() == mv.mKilledPiece->GetColour()) // blocked by same team
+      {
+        break;
+      }
+      mPseudoLegalMoves.push_back(mv);
+    }
+  }
+
   return mPseudoLegalMoves;
 }
 
